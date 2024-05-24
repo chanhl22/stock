2660a5e3b6e461e7d5755d84a55031d8ca2d19fa
chanhl22 <lch9502@naver.com>
2024-05-24 11:16:11 +0900

    feat: pessimisticLock

--- src/test/java/com/example/stock/service/PessimisticLockStockServiceTest.java
+++ src/test/java/com/example/stock/service/PessimisticLockStockServiceTest.java
@@ -14,7 +14,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PessimisticLockStockServiceTest {
